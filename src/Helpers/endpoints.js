b0e2e27e78533a75ed1281364defd0de630a1133
axlmaster38 <117498872+axlmaster38@users.noreply.github.com>
2023-02-17 19:21:50 -0500

    proveedor
    sirve el editar, visualizar y agregar

--- src/Helpers/endpoints.js
+++ src/Helpers/endpoints.js
@@ -2,12 +2,12 @@ const API_URL = 'http://localhost:8080';
 
 export const LOGIN_ENDPOINT = API_URL + "/users/login";
 export const REGISTER_ENDPOINT = API_URL + "/users";
-
 export const PRODUCTO_ENDPOINT = API_URL + "/producto";
 export const PRODUCTOID_ENDPOINT = API_URL + "/producto/";
-
 export const CATEGORIA_ENDPOINT = API_URL + "/categoria";
-
+export const PROVEEDORID_ENDPOINT = API_URL + "/proveedor/";
+export const PROVEEDOR_ENDPOINT = API_URL + "/proveedor/";
+export const REGISTER_PROVEEDOR_ENDPOINT = API_URL + "/proveedor";
 
 
 
